d745f8fad5386b6c383b51e930837381d1fbdfe3
Eric <eric2323223@gmail.com>
2018-03-02 15:58:24 +0800

    Update 3-train_subset.py

--- src/3-train_subset.py
+++ src/3-train_subset.py
@@ -12,17 +12,36 @@ from keras.layers.core import Dense, Dropout, Flatten
 from keras.models import Sequential
 from keras.regularizers import l2
 
+
+from keras import backend as K
+from os import environ
+from importlib import reload
+
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+# user defined function to change keras backend
+def set_keras_backend(backend):
+    if K.backend() != backend:
+       environ['KERAS_BACKEND'] = backend
+       reload(K)
+       assert K.backend() == backend
+
+
 if len(sys.argv) != 2:
-    print 'Usage: %s subset_filepath' % sys.argv[0]
+    print('Usage: %s subset_filepath' % sys.argv[0])
     sys.exit(1)
+
+# call the function with "theano"
+set_keras_backend("tensorflow")
 
 subset_filepath = sys.argv[1]
 
 # as described in http://yuhao.im/files/Zhang_CNNChar.pdf
 model = Sequential()
-model.add(Conv2D(64, (3, 3), weights=[np.random.normal(0, 0.01, size=(3, 3, 1, 64)), np.zeros(64)],
+model.add(Conv2D(64, (3, 3),
                         activation='relu', padding='same', strides=(1, 1),
-                        input_shape=(1, 64, 64)))
+                        input_shape=(64,64,1)))
 model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
 
 model.add(Conv2D(128, (3, 3), weights=[np.random.normal(0, 0.01, size=(3, 3, 64, 128)), np.zeros(128)],
@@ -33,6 +52,7 @@ model.add(Conv2D(256, (3, 3), weights=[np.random.normal(0, 0.01, size=(3, 3, 128
                         activation='relu', padding='same', strides=(1, 1)))
 model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
 
+# model.summary()
 model.add(Flatten())
 
 model.add(Dropout(0.5))
@@ -40,6 +60,8 @@ model.add(Dense(1024, activation='relu'))
 
 model.add(Dropout(0.5))
 model.add(Dense(200, activation='softmax'))
+
+model.summary()
 
 model.compile(loss='categorical_crossentropy', optimizer='adadelta', metrics=['accuracy'])
 
@@ -49,11 +71,20 @@ with open('%d-model.json' % timestamp, 'w') as f:
     json.dump(d, f, indent=4)
 
 with h5py.File(subset_filepath, 'r') as f:
-    model.fit(f['trn/x'], f['trn/y'], validation_data=(f['vld/x'], f['vld/y']),
+    print(f['trn/x'].shape)
+    #
+    # for i in range(5):
+    #     print("{}: {}".format(i, np.moveaxis(f['trn/x'].value, 1, i).shape))
+    converted_trn = np.moveaxis(f['trn/x'].value, 1, 3)
+    converted_vld = np.moveaxis(f['vld/x'].value, 1, 3)
+    converted_tst = np.moveaxis(f['tst/x'].value, 1, 3)
+
+    model.fit(converted_trn, f['trn/y'], validation_data=(converted_vld, f['vld/y']),
               epochs=15, batch_size=128, shuffle='batch', verbose=1)
 
-    score = model.evaluate(f['tst/x'], f['tst/y'], verbose=0)
-    print 'Test score:', score[0]
-    print 'Test accuracy:', score[1]
+    score = model.evaluate(converted_tst, f['tst/y'], verbose=0)
+    print('Test score:', score[0])
+    print('Test accuracy:', score[1])
 
     model.save_weights('%d-weights-%f.hdf5' % (timestamp, score[1]))
+
